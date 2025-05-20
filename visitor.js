651f46f019d8ab4236bd089b7c78147ff228dd57
webio21 <139540464+webio21@users.noreply.github.com>
2025-05-20 23:48:16 +0900

    Add files via upload

--- visitor.js
+++ visitor.js
@@ -2,6 +2,6 @@
   var clickyScript = document.createElement('script');
   clickyScript.src = "//static.getclicky.com/js";
   clickyScript.async = true;
-  clickyScript.setAttribute("data-id", "101485095");
+  clickyScript.setAttribute("data-id", "101485118");
   document.head.appendChild(clickyScript);
 })();
